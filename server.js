85c9c14d3194263586ca5048b8e091e3b25431dd
Aleks Kamko <aykamko@gmail.com>
2014-03-04 01:54:15 -0800

    FUCK YOU EXPRESS 3

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-var express = require('express')
+var express = require('express'),
+   partials = require('express-partials');
 var app = express();
-var md = require("node-markdown").Markdown;
+var md = require('node-markdown').Markdown;
+var ejs = require('ejs');
 var markedejs = require('markedejs');
-app.engine('.md', markedejs.__express);
+
+app.use(partials());
+// TODO: fix markdown
+// app.engine('.md', markedejs.__express);
 
 // Initialize main server
 app.use(express.json());
 app.use(express.urlencoded());
-
 app.use(express.static(__dirname + '/static'));
 
-app.set('view engine', 'md');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
@@ -24,3 +28,6 @@ app.get('/events', function(req, res){
 app.get('/music', function(req, res){
     res.render('music', {page: 'music'});
 });
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port);
