8ce1f27e599d640406e0235fdc884c7cd917dc79
Aleks Kamko <aykamko@gmail.com>
2014-03-04 00:07:03 -0800

    Fixed shit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var app = express.createServer();
+var app = express();
 var md = require("node-markdown").Markdown;
 var markedejs = require('markedejs');
 app.engine('.md', markedejs.__express);
@@ -7,19 +7,19 @@ app.engine('.md', markedejs.__express);
 // Initialize main server
 app.use(express.bodyParser());
 
-app.use(express.public(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 
 app.set('view engine', 'md')
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
     res.render('home', {page: 'home'});
-}
+});
 
 app.get('/events', function(req, res){
     res.render('events', {page: 'events'});
-}
+});
 
 app.get('/music', function(req, res){
     res.render('music', {page: 'music'});
-}
+});
