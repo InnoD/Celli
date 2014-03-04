9fde93eea1e48caf5ab5e05021e0dd13f41e139b
Aleks Kamko <aykamko@gmail.com>
2014-03-04 00:17:51 -0800

    wtflol

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var markedejs = require('markedejs');
 app.engine('.md', markedejs.__express);
 
 // Initialize main server
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use(express.static(__dirname + '/static'));
 
-app.set('view engine', 'md')
+app.set('view engine', 'md');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
