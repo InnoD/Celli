4a0cef32dd41947350f6d9c1726b1aee830fec75
Aleks Kamko <aykamko@gmail.com>
2014-03-04 00:00:08 -0800

    I think this works?

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express')
 var app = express.createServer();
 var md = require("node-markdown").Markdown;
+var markedejs = require('markedejs');
+app.engine('.md', markedejs.__express);
 
 // Initialize main server
 app.use(express.bodyParser());
 
 app.use(express.public(__dirname + '/static'));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'md')
 app.set('views', __dirname + '/views');
 
+app.get('/', function(req, res){
+    res.render('home', {page: 'home'});
+}
 
+app.get('/events', function(req, res){
+    res.render('events', {page: 'events'});
+}
+
+app.get('/music', function(req, res){
+    res.render('music', {page: 'music'});
+}
