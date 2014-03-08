3188de1ba87f49e7b9c602f5e8071e86c6fdbee0
qzshen <quinn.z.shen@gmail.com>
2014-03-08 03:15:30 -0800

    Added working ejs and script to pull events off page; doesn't pull all events because of paging bug

--- server.js
+++ server.js
@@ -1,7 +1,140 @@
 var express = require('express'),
-   partials = require('express-partials');
+  partials = require('express-partials');
 var app = express();
 var ejs = require('ejs');
+var https = require('https');
+var http = require('http');
+var url = require('url');
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+var events;
+
+// sort comparers for date strings
+function asorter(a, b) {
+  return getTime(a.start_time)-getTime(b.start_time);
+}
+
+function dsorter(a, b) {
+  return getTime(b.start_time)-getTime(a.start_time);
+}
+
+function formatDate(date) {
+  var d = date;
+  var hh = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  var dd = "AM";
+  var h = hh;
+  if (h >= 12) {
+      h = hh-12;
+      dd = "PM";
+  }
+  if (h == 0) {
+      h = 12;
+  }
+  m = m<10?"0"+m:m;
+  
+  if(m == '00') {
+    return h + dd;
+  }
+  
+  s = s<10?"0"+s:s;
+
+  var pattern = new RegExp("0?"+hh+":"+m+":"+s);
+  return h+":"+m+dd;
+}
+
+function getEvents(){
+  https.get({
+    host: 'graph.facebook.com',
+    path: '/466093040128127/events?access_token=658413130906919|jq0oT6M9dcNBf_a6EGXbpNHrWI4'
+  }, function(res){
+    var body = "";
+    var fully_explored = false;
+    res.on('data', function(chunk){
+      body += chunk;
+    });
+    res.on('end', function(){
+      if (body == false) {
+        return;
+      }
+      try{
+        var ts = (new Date()).valueOf();
+        events = {'upcoming': [], 'past': []};
+        // while(!fully_explored){
+          data = JSON.parse(body);
+          paging = data.paging;
+          data = data.data;
+
+          var event, date;
+          for (var i in data) {
+            event = data[i];
+            console.log(event.location);
+            // More detailed query
+            https.get({
+              host: 'graph.facebook.com',
+              path: '/' + event.id + '?fields=name,cover,description,start_time,id,location&access_token=658413130906919|jq0oT6M9dcNBf_a6EGXbpNHrWI4'
+            }, function(res){
+              body = "";
+              res.on('data', function(chunk){
+                body += chunk;
+              });
+              res.on('end', function(){
+                if(body == "false"){
+                  return;
+                }
+                try {
+                  event = JSON.parse(body);
+                } catch(e) {
+                  return;
+                }
+                date = new Date(event.start_time);
+                event.date = months[date.getMonth()] + " " + date.getDate();
+                event.dateObj = date;
+                event.time = formatDate(date);
+                if( event.description != undefined ) {
+                  event.description = event.description.replace(/(\r\n|\n|\r)/gm,' ');
+                }
+                event.pic_url = 'https://graph.facebook.com/' + event.id + '/picture?type=large';
+                if(event.dateObj.valueOf() > ts) {
+                  events['upcoming'].push(event);
+                } else {
+                  events['past'].push(event);
+                }
+
+                console.log(event);
+
+                // sort events.
+                events['upcoming'].sort(asorter);
+                events['past'].sort(dsorter);
+              });
+            });
+          // }
+          // if (paging != undefined && paging.next != undefined){
+          //   console.log(paging);
+          //   https.get(paging.next, function(res){
+          //     res.on('data', function(chunk){
+          //       body += chunk;
+          //     });
+          //     res.on('end', function(){
+          //       if (body == false) {
+          //         return;
+          //       }
+          //     });
+          //   }
+          // } else{
+          //   console.log('hihi');
+          //   fully_explored = true;
+          // }
+        }
+      } catch(e) {
+        return;
+      }
+    });
+  });
+}
+
+getEvents();
 
 app.use(partials());
 
@@ -18,7 +151,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/events', function(req, res){
-    res.render('events', {page: 'events'});
+    res.render('events', {page: 'events', events: events});
 });
 
 app.get('/music', function(req, res){
